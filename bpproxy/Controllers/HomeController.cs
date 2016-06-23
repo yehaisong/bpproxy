af5c5a90518810fe8029299bfafa3a6c508b720e
yehaisong <yehaisong@hotmail.com>
2016-06-23 11:41:35 -0400

    fisrt commit

--- bpproxy/Controllers/HomeController.cs
+++ bpproxy/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace bpproxy.Controllers
 {
@@ -13,6 +15,25 @@ namespace bpproxy.Controllers
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        public async Task<string> GetUserId(string email)
+        {
+            string responseString;
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                   { "email", email }
+                };
+
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await client.PostAsync("https://backpack.openbadges.org/displayer/convert/email", content);
+
+                responseString = await response.Content.ReadAsStringAsync();
+            }
+            return responseString;
         }
     }
 }
